66ffc409a4f7b013016929dcc0adee22e120eec5
vdegenne <vdegenne@gmail.com>
2022-03-10 21:12:29 +0100

    l to d

--- google-translate.js
+++ google-translate.js
@@ -1,7 +1,12 @@
 const voiceButtons = document.querySelectorAll('[aria-label="Translate by voice"]')
 const voiceButton = voiceButtons[voiceButtons.length - 1]
+const listenButtons = document.querySelectorAll('[aria-label="Listen to source text"]')
+const listenButton = listenButtons[listenButtons.length - 1]
 window.addEventListener('keydown', function (e) {
   if (e.altKey && (e.key === 's' || e.key === 'S')) {
     voiceButton.click()
+  }
+  if (e.altKey && (e.key === 'd' || e.key === 'd')) {
+    listenButton.click()
   }
 })
