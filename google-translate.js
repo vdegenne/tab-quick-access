4e237ec31e3b3e076113cfc05098120f72c838c1
vdegenne <vdegenne@gmail.com>
2022-03-13 13:58:37 +0100

    interchange s and d google translate

--- google-translate.js
+++ google-translate.js
@@ -3,10 +3,12 @@ const voiceButton = voiceButtons[voiceButtons.length - 1]
 const listenButtons = document.querySelectorAll('[aria-label="Listen to source text"]')
 const listenButton = listenButtons[listenButtons.length - 1]
 window.addEventListener('keydown', function (e) {
-  if (e.altKey && (e.key === 's' || e.key === 'S')) {
+  // D for Dictate
+  if (e.altKey && (e.key === 'd' || e.key === 'D')) {
     voiceButton.click()
   }
-  if (e.altKey && (e.key === 'd' || e.key === 'D')) {
+  // S for Speak text
+  if (e.altKey && (e.key === 's' || e.key === 'S')) {
     e.preventDefault()
     listenButton.click()
   }
